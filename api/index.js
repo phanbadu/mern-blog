5a64c4b39fa35510b94d1a3d43f85024f9966000
phanbadu <duphan03@gmail.com>
2024-02-22 09:05:07 +0700

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import epxress from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(() => console.log("MongoDb is connected"))
+    .catch((err) => console.log(err))
 
 const app = epxress();
 
