a4bff799cc36e90dc22fa56d33e74e9c5e2e4438
phanbadu <duphan03@gmail.com>
2024-03-01 04:57:30 +0700

    add posts section to the dashbroad

---
+++ client/src/components/DashPosts.jsx
@@ -0,0 +1,9 @@
+
+
+export default function DashPosts() {
+    return (
+        <div>
+            aaaaaaa
+        </div>
+    )
+}
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -11,10 +11,11 @@ import {
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { signoutSuccess, } from '../redux/user/userSlice';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector} from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function DashSidebar() {
+    const { currentUser } = useSelector(state => state.user);
     const location = useLocation();
     const [tab, setTab] = useState("");
     const dispatch = useDispatch();
@@ -27,7 +28,7 @@ export default function DashSidebar() {
         }
     }, [location.search]);
 
-    
+
     const handleSignout = async () => {
         try {
             const res = await fetch("/api/users/signout", {
@@ -51,12 +52,19 @@ export default function DashSidebar() {
     return (
         <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
-                <Sidebar.ItemGroup>
+                <Sidebar.ItemGroup className="flex flex-col gap-1">
                     <Link to="/dashboard?tab=profile">
-                        <Sidebar.Item active={tab === "profile"} icon={HiUser} label={"User"} labelColor="dark" as="div">
+                        <Sidebar.Item active={tab === "profile"} icon={HiUser} label={currentUser.isAdmin ? "Admin" : "User"} labelColor="dark" as="div">
                             Profile
                         </Sidebar.Item>
                     </Link>
+                    {currentUser.isAdmin && (
+                        <Link to="/dashboard?tab=posts">
+                            <Sidebar.Item active={tab === "posts"} icon={HiDocumentText} as="div">
+                                Posts
+                            </Sidebar.Item>
+                        </Link>
+                    )}
                     <Sidebar.Item onClick={handleSignout} icon={HiArrowSmRight} className="cursor-pointer">
                         Sign out
                     </Sidebar.Item>
