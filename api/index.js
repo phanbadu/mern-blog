b5f2d1403f6fdc989cebf46a1f2bc46bc1e71a5e
phanbadu <duphan03@gmail.com>
2024-03-02 01:15:42 +0700

    show user posts inside dashbroad

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 
-app.use((err, req, res, nex) => {
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
     res.status(statusCode).json({
