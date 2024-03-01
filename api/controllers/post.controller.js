134235628617bd8a4366cf32b4b9b01ed24b8dde
phanbadu <duphan03@gmail.com>
2024-03-02 02:58:34 +0700

    create get users api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,19 +1,23 @@
-import Post from "../models/post.model.js";
-import { errorHandler } from "../utils/error.js";
+import Post from '../models/post.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const create = async (req, res, next) => {
     if (!req.user.isAdmin) {
-        return next(errorHandler(403, "You are not allowed to create a post"));
+        return next(errorHandler(403, 'You are not allowed to create a post'));
     }
     if (!req.body.title || !req.body.content) {
-        return next(errorHandler(403, "Please provide all required fields"));
+        return next(errorHandler(400, 'Please provide all required fields'));
     }
-
-    const slug = req.body.title.split(" ").join("-").toLowerCase().replace(/[^a-zA-Z0-9-]/g, "-");
+    const slug = req.body.title
+        .split(' ')
+        .join('-')
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9-]/g, '');
     const newPost = new Post({
-        ...req.body, slug, userId: req.user.id,
+        ...req.body,
+        slug,
+        userId: req.user.id,
     });
-
     try {
         const savedPost = await newPost.save();
         res.status(201).json(savedPost);
@@ -69,12 +73,11 @@ export const getposts = async (req, res, next) => {
 
 export const deletepost = async (req, res, next) => {
     if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-        return next(errorHandler(403, "You are not allowed to delete this post"));
+        return next(errorHandler(403, 'You are not allowed to delete this post'));
     }
-
     try {
         await Post.findByIdAndDelete(req.params.postId);
-        res.status(200).json("The post has been deleted");
+        res.status(200).json('The post has been deleted');
     } catch (error) {
         next(error);
     }
